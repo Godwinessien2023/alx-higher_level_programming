d5f73b475a197938b7d4373385e3fd45249fc01a
Godwinessien2023 <kinggodwin4u@yahoo.com>
2023-12-29 13:06:45 +0000

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def no_c(my_string):
+	store = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            store += my_string[i]
+    return (store)i
