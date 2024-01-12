5bb68e4234e67614e81018a48755de6b626aca53
Godwin Essien <138781331+Godwinessien2023@users.noreply.github.com>
2024-01-12 18:55:09 +0100

    Delete 0x00-python-hello_world directory

--- 0x00-python-hello_world/5-print_string.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-str = "Holberton School"
-print(f"{str * 3}")
-first_9 = str[:9]
-print(first_9)
--- 0x00-python-hello_world/README.md
+++
@@ -1,1 +0,0 @@
-Thhis readme is hereby created for the directory 0x00 python  hello world.
