fd329e0357224ca1ed4f3bbc8f9b1aced4c3f055
Godwin Essien <kinggodwin4u@yahoo.com>
2024-01-22 11:53:19 -0800

    calculator now woking

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,12 +2,9 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
-# define variables for a and b
     a = 10
     b = 5
-# print answers
-
-   print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
