7fe8a34619b33e6ef5d5ea73a6d466dc03ebd5ea
Godwin Essien <kinggodwin4u@yahoo.com>
2024-01-22 11:18:41 -0800

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,10 +5,10 @@ if __name__ == " __main__":
     a = 10
     b = 5
 # represent each calculation
-    sum_result = add(a, b)
-    sub_result = sub(a, b)
-    mul_result = mul(a, b)
-    div_result = div(a, b)
+    sum_result = calculator_1.add(a, b)
+    sub_result = calculator_1.sub(a, b)
+    mul_result = calculator_1.mul(a, b)
+    div_result = calculator_1.div(a, b)
 # print answers
     print(f"a + b = {sum_result}")
     print(f"a - b = {sub_result}")
