e41b45e88811a797ee724fc732f9cd6d0d89b639
Godwin Essien <kinggodwin4u@yahoo.com>
2024-01-22 07:29:03 -0800

    trying

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+if __name__ == " __main__":
+    import calculator_1
+# define variables for a and b
+    a = 10
+    b = 5
+# represent each calculation
+    sum_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
+# print answers
+    print(f"a + b = {sum_result}")
+    print(f"a - b = {sub_result}")
+    print(f"a * b = {mul_result}")
+    print(f"a / b = {div_result}")
