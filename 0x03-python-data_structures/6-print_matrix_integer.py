aa8b6675ce9aee01d02e35d3f0d77bdb84dc74fe
Godwin Essien <kinggodwin4u@yahoo.com>
2024-01-27 16:46:33 +0000

    integer matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+        for i in row:
+            print("{:d}".format(i), end=" " if num != row[-1] else "")
         print()
