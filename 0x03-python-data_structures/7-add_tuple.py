bf90a5ecd010efc5acadf37eb8f93278c595bded
Godwin Essien <kinggodwin4u@yahoo.com>
2024-01-27 19:03:22 +0000

    add tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3       
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract the first and second elements from each tuple (or use 0 if not present)
+    # Extract the first and second elements from each tuple)
     a_first = tuple_a[0] if len(tuple_a) >= 1 else 0
     a_second = tuple_a[1] if len(tuple_a) >= 2 else 0
     b_first = tuple_b[0] if len(tuple_b) >= 1 else 0
