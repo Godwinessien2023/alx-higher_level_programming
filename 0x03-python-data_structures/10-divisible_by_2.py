03240e9152164dfa590a390576ff0bf821795cfe
Godwin Essien <kinggodwin4u@yahoo.com>
2024-01-29 08:40:15 +0000

    divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+
+    # Itterate my_list
+    new_list = []
+
+    # evaluation statements to show divisible by 2
+    for i in my_list:
+        if i % 2 == 0:
+            my_list.append(True)
+        else:
+            my_list.append(False)
+
+    return new_list
