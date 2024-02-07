777205ace00e21ca6c3c11d54bc9dad7573eb7d0
Godwin Essien <kinggodwin4u@yahoo.com>
2024-02-07 23:25:52 +0000

    done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,3 @@
-
-#!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        if (isinstance(value, int)):
-            print("{:d}".format(value))
-            return True
-        else:
-            return False
-    except Exception as e:
-        return e
 #!/usr/bin/python3
 
 def safe_print_integer(value):
