5d2bdeaed9b06d3b02c580948e55a0ac9ba1239f
Godwin Essien <kinggodwin4u@yahoo.com>
2024-02-07 23:11:49 +0000

    print digit

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        for i in range(len(value)):
+            if isinstance(value[i], int):
+                print("{:d}".format(value[i]), end="")
+        print() 
+        return True
+    except ValueError:
+        print()
+        return False
