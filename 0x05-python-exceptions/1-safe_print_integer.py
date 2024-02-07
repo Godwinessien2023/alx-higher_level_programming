f9a9c165e8bb160daafd6afa4455accb94df4575
Godwin Essien <kinggodwin4u@yahoo.com>
2024-02-07 23:14:17 +0000

    print digit

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     try:
         for i in range(len(value)):
-            if isinstance(value[i], int):
+            if isdigit(value[i], int):
                 print("{:d}".format(value[i]), end="")
         print() 
         return True
