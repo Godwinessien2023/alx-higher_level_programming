a30f6092ba0d9b4a7aa34bc1c26152aa460fbb1d
Godwin Essien <kinggodwin4u@yahoo.com>
2024-02-20 02:00:44 +0000

    log out

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+""" This is a class for a swuare"""
+
+
+class Square:
+
+    """This tim with an objecy attribute"""
+    def __init__(self, size=0):
+        self.size = size
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+    elif:
+        if size < 0:
+        raise ValueError(size must be >= 0)
