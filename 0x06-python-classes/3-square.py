10e10500b84f1bcd66f4eaf522c41fee007c1e0c
Godwin Essien <138781331+Godwinessien2023@users.noreply.github.com>
2024-02-20 13:15:27 +0100

    class Square with object attributes of size and area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""define a class"""
+class Square:
+    """ define object of the class"""
+    def __init__(self, size=0):
+        self.__size = size
+
+        """define if size is an integer"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif: size < 0:
+            raise ValueError("size must be >= 0")
+    
+    """define area"""
+    def area(self):
