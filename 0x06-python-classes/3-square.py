97977b29102993bd5a08669c1ab50d0523bb8067
Godwin Essien <138781331+Godwinessien2023@users.noreply.github.com>
2024-02-20 13:29:34 +0100

    First time using vs code for project so ia m trying to meet up

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,11 +4,11 @@
 
 class Square:
     """ define object of the class"""
-    def __init__(self, size=0):
 
+
+    def __init__(self, size=0):
         """initialize an instance of the class"""
         self.__size = size
-
         """define if size is an integer"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -17,4 +17,5 @@ class Square:
     
     """define area"""
     def area(self):
+        """return the area of the square"""
         return (self.size **2)
