c1c2cd957d48f6872ca33946be5d2cd8f9794abf
Godwin Essien <kinggodwin4u@yahoo.com>
2024-02-20 01:23:31 +0000

    object attribut

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+""" This is a class for squares, this time with am attribute"""
+
+
+class Square:
+    """ the attribute object is a dimention"""
+    
+    def __init__(self, size):
+        self.__size = size
+    pass
