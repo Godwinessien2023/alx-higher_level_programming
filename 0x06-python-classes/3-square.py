0b54167938b33c5c5a0e5f0490ee24041d0a9bb0
Godwin Essien <138781331+Godwinessien2023@users.noreply.github.com>
2024-02-20 13:40:12 +0100

    this is proving difficult

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""define a class"""
+""" Define a Square"""
 
 
 class Square:
     """ define object of the class"""
 
-
     def __init__(self, size=0):
         """initialize an instance of the class"""
         self.__size = size
-        """define if size is an integer"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif: size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
     
-    """define area"""
     def area(self):
         """return the area of the square"""
         return (self.size **2)
