4016137ae8bd98c283fbd787c5c89831f1a06c74
Godwin Essien <kinggodwin4u@yahoo.com>
2024-02-20 02:10:39 +0000

    sizing up

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,10 +7,10 @@ class Square:
 
     """This tim with an objecy attribute"""
     def __init__(self, size=0):
+        """ initialixes a class attribute"""
         self.size = size
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
-    elif:
-        if size < 0:
+    elif size < 0:
         raise ValueError(size must be >= 0)
