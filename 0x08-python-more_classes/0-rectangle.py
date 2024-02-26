68d18ab1d712c5a2883b974a3ee36fa794f8f18b
Godwin Essien <kinggodwin4u@yahoo.com>
2024-02-26 01:30:04 +0100

    simple rectangle class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 class Rectangle:
-    """defines a class rectangle"""
+    """
+    This is a simple Rectangle class.
+
+    Attributes:
+    - width (int): The width of the rectangle.
+    - height (int): The height of the rectangle.
+    """
     pass
