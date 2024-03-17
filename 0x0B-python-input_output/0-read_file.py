279ef671838ecc61ca619c3970ac5d1b3afab5ac
Godwin Essien <kinggodwin4u@yahoo.com>
2024-03-17 05:00:55 +0100

    File input/output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout.
+    Args:
+        filename (str): The name of the file
+    """
+    with open(filename, encoding="UTF8") as f:
+        print(f.read(), end="")
