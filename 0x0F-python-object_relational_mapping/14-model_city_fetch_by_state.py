0a062178983b1f88d96cfb91d9f0de746a90d81c
Godwin Essien <kinggodwin4u@yahoo.com>
2024-04-29 14:39:18 +0100

    task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,8 +5,8 @@ Prints all City objects from the database hbtn_0e_14_usa
 import sys
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from SQLAlchemy import create_engine
+from SQLAlchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
