9bd27a2d2fdf60b2fd592b22551d0345e7c1e209
Godwin Essien <kinggodwin4u@yahoo.com>
2024-06-13 21:11:47 +0100

    task 2 & 3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+const url = process.argv[2];
+request.get(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:' + ' ' + response.statusCode);
+  }
 });
