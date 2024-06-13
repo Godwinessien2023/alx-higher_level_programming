60a8e11545475eb9287fe9c01a27432a45fac2d6
Godwin Essien <kinggodwin4u@yahoo.com>
2024-06-13 21:31:31 +0100

    task 2 retouched

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
-
 const request = require('request');
+if (process.argv.length < 3) {
+  console.error('Usage: node statusCode.js <URL>');
+  process.exit(1);
+}
 const url = process.argv[2];
-request.get(url, function (error, response) {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    console.log('code:' + ' ' + response.statusCode);
+    console.error('Error making request:', error);
+    process.exit(1);
   }
+  console.log(`code: ${response.statusCode}`);
 });
