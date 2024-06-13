152004d813ad28a0a11cdc7e4699a1f6c2ac6882
Godwin Essien <kinggodwin4u@yahoo.com>
2024-06-13 21:01:57 +0100

    task 2 and 3

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-// Write content to the file
-fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
-  if (err) {
-    console.log(err);
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
