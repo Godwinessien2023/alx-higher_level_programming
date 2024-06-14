62387cd02bc3503f7b813124b29d733363a6aba0
Godwin Essien <kinggodwin4u@yahoo.com>
2024-06-14 09:20:49 -0700

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const movieId = process.argv[2];
+const endPoint = 'https://swapi-api.alx-tools.com/api/films/';
+// Make a GET request to the Star Wars API
+request.get(endPoint + movieId, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
 });
