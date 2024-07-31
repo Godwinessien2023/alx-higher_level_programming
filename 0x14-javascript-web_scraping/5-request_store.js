e33820b0dc902df2a76c8bbff898d6816e0bcd32
Godwin Essien <kinggodwin4u@yahoo.com>
2024-07-31 22:11:56 +0100

    task 5 please naw

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,17 +1,24 @@
 #!/usr/bin/node
-
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
+
+// Get the URL and file path from the command line arguments
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request.get(url, (error, response, body) => {
+// Make a GET request to the provided URL
+request(url, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error('An error occurred:', error);
+    return;
   }
-  fs.writeFile(filePath, body, 'utf8', (err, data) => {
+
+  // Write the body response to the file
+  fs.writeFile(filePath, body, 'utf8', (err) => {
     if (err) {
-      console.log(err);
+      console.error('An error occurred while writing to the file:', err);
+      return;
     }
+    console.log('File has been written successfully');
   });
 });
