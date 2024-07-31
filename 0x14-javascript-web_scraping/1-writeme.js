342a0c3b01448bd2b4ca091fcee4ced6b32c6c46
Godwin Essien <kinggodwin4u@yahoo.com>
2024-07-31 02:51:43 +0100

    advance

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+#!/usr/bin/node
+const fs = require('fs');
+const path = require('path')
+
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+if (!filePath) {
+    console.error('Usage: node script_name.js <file_path> <content_to_write>' );
+    process.exit(1);
+}
+
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+    if(err) {
+        console.error('An error Occured:', err);
+        return;
+    }
+    console.log(data);
+});
