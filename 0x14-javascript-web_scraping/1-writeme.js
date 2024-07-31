9a36d38c6dca2ba76cc2c64d1ed9da64f3adefa8
Godwin Essien <kinggodwin4u@yahoo.com>
2024-07-31 18:35:13 +0100

    shaped the code a lil bit

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
-
+// Write content to the file
 fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
   if (err) {
-    console.error('An error Occured:', err);
-    return;
+    console.log(err);
   }
-  console.log('file has been written sucessfuly');
 });
