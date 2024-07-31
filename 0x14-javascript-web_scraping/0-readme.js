87054ca8b2f867586abacfd9001260ec0281e6e0
Godwin Essien <kinggodwin4u@yahoo.com>
2024-07-31 02:28:29 +0100

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const fs = require('fs');
+const path = require('path')
+
+const filePath = process.argv[2];
+
+if (!filePath) {
+    console.error('Usage: node script_name.js <file_path>');
+    process.exit(1);
+}
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if(err) {
+        console.error('An error Occured:', err);
+        return;
+    }
+    console.log(data);
+});
