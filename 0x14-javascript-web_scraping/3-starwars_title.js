435444cf5d80163f3bd084a3bbeb7b5311c24951
Godwin Essien <kinggodwin4u@yahoo.com>
2024-07-31 21:44:56 +0100

    semistandard fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,6 +6,10 @@ const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(apiUrl, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error('An error occurred:', error);
+    return;
+  }
   if (response.statusCode !== 200) {
     console.error(`Failed to fetch data: ${response.statusCode}`);
     return;
