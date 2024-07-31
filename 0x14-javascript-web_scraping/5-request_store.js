54aa6e7bf01e2ef17172b4b0f4310d5ddb0be275
Godwin Essien <kinggodwin4u@yahoo.com>
2024-07-31 22:08:36 +0100

    done

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,28 +1,17 @@
 #!/usr/bin/node
-const request = require('request');
-const fs = require('fs');
 
-// Get the URL and file path from the command line arguments
+const fs = require('fs');
+const request = require('request');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// Make a GET request to the provided URL
-request(url, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error('An error occurred:', error);
-    return;
+    console.log(error);
   }
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data: ${response.statusCode}`);
-    return;
-  }
-
-  // Write the body response to the file
-  fs.writeFile(filePath, body, 'utf8', (err) => {
+  fs.writeFile(filePath, body, 'utf8', (err, data) => {
     if (err) {
-      console.error('An error occurred while writing to the file:', err);
-      return;
+      console.log(err);
     }
-    console.log('File has been written successfully');
   });
 });
