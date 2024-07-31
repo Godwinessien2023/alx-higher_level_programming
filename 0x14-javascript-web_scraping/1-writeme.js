46bfda9db141f4b591c7362cd5357a12a9be9a64
Godwin Essien <kinggodwin4u@yahoo.com>
2024-07-31 12:52:56 +0100

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
-#!/usr/bin/node
 const fs = require('fs');
-const path = require('path')
 
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
-if (!filePath) {
-    console.error('Usage: node script_name.js <file_path> <content_to_write>' );
-    process.exit(1);
+if (!filePath || !contentToWrite) {
+  console.error('Usage: node script_name.js <file_path> <content_to_write>');
+  process.exit(1);
 }
 
 fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
-    if(err) {
-        console.error('An error Occured:', err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error('An error Occured:', err);
+    return;
+  }
+  console.log('file has been written sucessfuly');
 });
