bbdbd063c5a96c07328eb144cae2cb94bf1f1b11
Godwin Essien <kinggodwin4u@yahoo.com>
2024-07-31 18:54:18 +0100

    task 4

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const request = require('request');
+
+
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, { json: true }, (error, response, body) => {
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch data: ${response.statusCode}`);
+    return;
+  }
+  console.log(body.title);
+});
